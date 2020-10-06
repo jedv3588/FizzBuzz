aeaefa567ba359eb462413728fa159fdb3a15da5
jedv3588 <68433228+jedv3588@users.noreply.github.com>
2020-10-06 02:47:43 -0500

    Add files via upload

---
+++ fizzbuzz_php_TDD.php
@@ -0,0 +1,33 @@
+<?php
+$max_num = 100;
+
+    function fizz_buzz($n){
+
+        $output = '';
+        
+            switch(true){
+
+                case($n % 15 === 0):
+                    $output .= "FizzBuzz"."\n";
+                    break;
+        
+                case($n % 3 === 0 ):
+                    $output .= "Fizz"."\n";
+                    break;
+        
+                case($n % 5 === 0 ):
+                    $output .= "Buzz"."\n";
+                    break;
+        
+                default:
+                    $output .= $n."\n";
+                    break;
+            }
+        
+        return $output;
+    }
+
+for($i = 1; $i <= $max_num; $i++){
+    echo $i." ".fizz_buzz($i);
+    }
+?>
